b98721a04d3c72d7e609dd6533f05fcb09366f7b
Jonathan <jonathan.pastor@me.com>
2015-04-17 00:38:10 +0200

    add a request buffer in order to avoid duplicate queries at the same time; (8)

--- lib/rome/utils/MemoizationDecorator.py
+++ lib/rome/utils/MemoizationDecorator.py
@@ -30,24 +30,25 @@ class MemoizationDecorator(object):
 
         def compute_hash(self, method_name, *args, **kwargs):
             import random
-            hash = random.getrandbits(128)
-            # hash = hash("%s_%s_%s" % (method_name, args, kwargs))
-            return hash
+            # hashv = random.getrandbits(128)
+            hashv = hash("%s_%s_%s" % (method_name, args, kwargs))
+            print("compute_hash: %s => %s" % ("%s_%s_%s" % (method_name, args, kwargs), hashv))
+            return hashv
 
         def __call__(self, *args, **kwargs):
 
             call_hash = self.compute_hash(self.method_name, args, kwargs)
-            self.memory[call_hash] = {
-                "modification_lock": threading.Lock(),
-                "result_queue": Queue.Queue(),
-                "result": None,
-                "waiting_threads_count": 0,
-                "closed": False
-            }
-
-            result = self.callable_object(*args, **kwargs)
-            self.memory[call_hash]["result"] = result
-            self.memory[call_hash]["result_queue"].put(result)
+            # self.memory[call_hash] = {
+            #     "modification_lock": threading.Lock(),
+            #     "result_queue": Queue.Queue(),
+            #     "result": None,
+            #     "waiting_threads_count": 0,
+            #     "closed": False
+            # }
+            #
+            # result = self.callable_object(*args, **kwargs)
+            # self.memory[call_hash]["result"] = result
+            # self.memory[call_hash]["result_queue"].put(result)
 
             if call_hash in self.memory:
                 # Increment safely the number of threads waiting for expected value
