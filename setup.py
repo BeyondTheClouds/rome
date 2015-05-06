8622c745f65891cbd19705f1c3bb3a7e54daae46
Jonathan <jonathan.pastor@me.com>
2015-05-06 16:23:38 +0200

    trying to merge changes;

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     packages=['lib', 'lib.rome', 'lib.rome.core', 'lib.rome.core.orm', 'lib.rome.core.dataformat', 'lib.rome.driver',
               'lib.rome.driver.riak', 'lib.rome.driver.redis', 'lib.rome.utils', 'lib.rome.conf', 'lib.rome.engine',
-              'test'],
+              'lib.rome.core.expression', 'test'],
     url='https://github.com/badock/rome',
     license='',
     author='jonathan',
