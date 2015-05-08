8343f7d5d0908148a650265d314122187f4387ca
Jonathan <jonathan.pastor@me.com>
2015-05-08 20:34:41 +0200

    trying to merge changes; (5)

--- lib/rome/core/utils.py
+++ lib/rome/core/utils.py
@@ -94,9 +94,9 @@ def get_objects(tablename, desimplify=True, request_uuid=None, skip_loading=Fals
     from lib.rome.core.dataformat.deconverter import JsonDeconverter
     object_deconverter = JsonDeconverter(request_uuid=request_uuid)
     data = database_driver.get_driver().getall(tablename, hints=hints)
-    # result = map(lambda x: transform(x, object_deconverter, skip_loading), data)
-    # return result
-    return data
+    result = map(lambda x: transform(x, object_deconverter, skip_loading), data)
+    return result
+    # return data
 
 
 def get_models_satisfying(tablename, field, value, request_uuid=None, hints=[]):
